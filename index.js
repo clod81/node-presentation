a3aaa5c217a07f64d40033656ce17044733325c2
Claudio Contin <claudiocontin@mint>
2014-08-26 12:16:18 +1200

    step 2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 http = require ('http');
 
-http.createServer().listen(3000, '127.0.0.1');
+http.createServer(function(req, res){ // CALLBACK
+  console.log("a request just arrived!");
+  res.writeHead(200, {"ContentType": "text/plain"});
+  res.end("Hi there!");
+}).listen(3000, '127.0.0.1');
 
 console.log("I am running now");
