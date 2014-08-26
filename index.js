e6df4373c7cf52dd6e2860e6aefe21f99a0966e6
Claudio Contin <claudiocontin@mint>
2014-08-26 12:49:48 +1200

    try withtimeout

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ http = require ('http');
 
 http.createServer(function(req, res){ // CALLBACK
   console.log("a request just arrived!");
-  res.writeHead(200, {"ContentType": "text/plain"});
-  res.end("Hi there!");
+  setTimeout(function(){
+   res.writeHead(200, {"ContentType": "text/plain"});
+   res.end("Hi there!");
+  }, 5000);
 }).listen(3000, '127.0.0.1');
 
 console.log("I am running now");
