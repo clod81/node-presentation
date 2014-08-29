4a6dfecee855f5a2881d08f2365a47007b8d5e1a
Claudio Contin <claudiocontin@mint>
2014-08-29 13:44:02 +1200

    fix branch 2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-http = require ('http');
+http = require('http');
 
 http.createServer(function(req, res){ // CALLBACK
   console.log("a request just arrived!");
   res.writeHead(200, {"ContentType": "text/plain"});
   res.end("Hi there!");
-}).listen(3000, '127.0.0.1');
+}).listen(3000);
 
 console.log("I am running now");
